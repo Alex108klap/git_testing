044584f09af155a2844c722e0287954fdc88223f
Alex108klap <144609469+Alex108klap@users.noreply.github.com>
2024-03-17 16:52:29 +0100

    Добавлена функция умножения и удален text

--- first/nuns.py
+++ first/nuns.py
@@ -1,13 +1,15 @@
 def plus(a, b):
     return a + b
 
+def mul(a,b):
+    return a*b
 
-text = "New text"
 
 def minus(a, b):
     return a - b
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     print(plus(2,2))
     print(minus(4,2))
+    print(mul(3,3))
